c52b29166564f1120a26b13314885f688a588fb1
sataratimoori <sataratimoori@gmail.com>
2026-09-07 09:59:24 +0430

    Add JavaScript variables and data types

---
+++ main.js
@@ -0,0 +1,37 @@
+const fName = "Satara";
+
+const lName = "Timoori";
+
+let age = 19;
+
+let countiry = "Afghanistan"
+
+let city = "Herat";
+
+let student = true;
+
+let grade = null;
+
+let fatherName ;
+
+const id = 456321;
+
+let present = true;
+
+let gender = "Female";
+
+let language = "JavaScript" ;
+
+let gmail = "sataratimoori@gmail.com";
+
+let score = null ;
+
+let phone = "0704 37 16 22";
+
+let course = "Web";
+
+let hobby = "coding";
+
+let time = "10:30 12:30";
+
+let teacher = "solimanKhil";
