01995b5ad2e9d9cc3f43f8a1e795d86cfa329e6d
sataratimoori <sataratimoori@gmail.com>
2026-09-07 11:14:24 +0430

    Practice assignment operators

---
+++ operators.js
@@ -0,0 +1,25 @@
+let a = 45;
+
+let b = 12;
+
+let c = 54;
+
+let d = true;
+
+let e = false;
+
+console.log(a++);
+console.log(++a);
+
+console.log(a--);
+console.log(--a);
+
+console.log(a += b);
+
+console.log(c -= a);
+
+console.log(b /= a);
+
+console.log(c *= b);
+
+console.log(a **= b);
