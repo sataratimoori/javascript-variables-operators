80545882efd339e027ba45bbd1040021dccea71e
sataratimoori <sataratimoori@gmail.com>
2026-09-07 10:03:19 +0430

    Add arithmetic operators practice

--- main.js
+++ main.js
@@ -48,3 +48,38 @@ console.log(fatherName);
 console.log(id);
 console.log(present);
 console.log(gender);
+
+
+
+
+
+let a = 23;
+
+let b = 78;
+
+let c = 12;
+
+let d = 98;
+
+let e = 90;
+
+let f = 65;
+
+let g = 54;
+
+
+
+console.log("'Arithmetic Assignment'");
+
+console.log(a + g);
+console.log(g - e);
+console.log(f * c);
+console.log(e / a);
+console.log(d ** f);
+console.log(e % a);
+console.log(c + d);
+console.log(f - b);
+console.log(b * g);
+console.log(g / c);
+console.log(f ** d);
+console.log(b % a);
